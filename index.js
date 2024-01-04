f4d5d4f175a06c89933cc88a8976ee4d2eb4a6e1
JugalAubie <jugal@auberginesolutions.com>
2024-01-04 12:26:36 +0530

    added readme file

---
+++ index.js
@@ -0,0 +1,13 @@
+const quotes = [
+    "Stay hungry, stay foolish. - Steve Jobs",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+    "Stay hungry, stay foolish. - Steve Jobs",
+    "Life is what happens when you're busy making other plans. - John Lennon",
+  ];
+  
+  module.exports = function () {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+  };
